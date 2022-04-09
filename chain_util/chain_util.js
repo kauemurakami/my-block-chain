8c8ca48f674a5136b0f73cb33f7fd559acdd0fe6
unknown <kauetmurakami@gmail.com>
2022-04-09 11:33:47 -0300

    feat: create module transaction

--- chain_util/chain_util.js
+++ chain_util/chain_util.js
@@ -1,8 +1,12 @@
-// metodos que vao criar hashs/chaves unicas 
+// metodos que vao criar hashs/chaves unicas /uuid
 //todos os dados de criptografia
 //utils
 
 const EC = require('elliptic').ec
+
+//uuid v1 possui o timestamp no método de criação, oprtanto iremos utiliza-la
+// garantindo a unicidade do uuid
+const uuidV1 = require('uuid/v1')
 
 // algoritmo de criptografia de curva eliptica
 // modelo que o bitcoin utiliza
@@ -10,6 +14,10 @@ const ec = new EC('secp256k1')
 
 class ChainUtil {
 
+
+    static id(){
+        return uuidV1()
+    }
 
     static genKeyPair(){
         return ec.genKeyPair()
---
+++ transaction/index.js
@@ -0,0 +1,28 @@
+//transação de carteira <-> carteira
+const ChainUtil = require('../chain_util')
+class Transaction{
+
+  constructor(){
+    this.id = ChainUtil.id()
+    this. input = null
+    this.outputs = []
+  }
+  /*
+    > sendWallet = objeto carteira do remetente, 
+    vamos utilizar campos importantes como endereço da carteira e balance (saldo)
+    > recipient= endereço da carteira que irá receber a transação
+    > amount = quantia que será enviada do sender -> recipient
+  */
+  static newTransaction(senderWallet, recipient, amount){
+    const transaction = new this()
+    if(senderWallet.balance < amount){
+      console.log(`
+      Amount: ${amount} exceeds balance`)
+      return
+    }
+
+    
+
+  }
+
+}
