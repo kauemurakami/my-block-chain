d8a22b9ac9ee18e0ebfb86cf73331c2389287029
unknown <kauetmurakami@gmail.com>
2022-04-09 11:20:27 -0300

    install uuid for unique id transaction

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 const DIFFICULTY = 4
 const MINE_RATE = 3000 // 3mil milisegundos || 3 segundos
 const INITIAL_BALANCE = 0;
-module.exports = {DIFFICULTY, MINE_RATE}
+module.exports = {DIFFICULTY, MINE_RATE, INITIAL_BALANCE}
--- wallet/index.js
+++ wallet/index.js
@@ -18,6 +18,6 @@ class Wallet {
     }
 
     //criptografia eliptica
-
-
 }
+
+module.exports = Wallet
